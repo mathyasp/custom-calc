84e71d932dfb07043dc3f132ef6197d68e093a45
Mathyas Papp <119142489+mathyasp@users.noreply.github.com>
2023-08-23 17:00:24 -0400

    Added main code to custom-calc.py

--- custom-calc.py
+++ custom-calc.py
@@ -5,7 +5,20 @@
 
 # It should print a prompt that makes it clear what 
 # is being calculated. 
+from math import sqrt
+print("Pythagorean Theorem Calculator")
+right_triangle = input("Does your triangle have one 90 degree angle? \n").lower()
 
+if right_triangle == "yes":
+    length_a = input("What is the length of side a? \n")
+    length_b = input("What is the length of side b? \n")
+    c_squared = (int(length_a))**2 + ((int(length_b))**2)
+    c = sqrt(c_squared)
+    print(f"Your hypotenuse is {c}") 
+else:
+    print("I'm sorry, I am just a pythagorean theorem calculator")
+
+   
 # For example: 
 # Enter height, width, and length to calculate the area of a cube
 # Height: 3
